0aa7f75df2e6fc34fde799bb52d2afc355118cb5
samkosal <67755908+samkosal@users.noreply.github.com>
2026-08-19 23:44:36 -0700

    finished approach 2

--- easy/PalindromeNumber.java
+++ easy/PalindromeNumber.java
@@ -128,7 +128,20 @@ public class PalindromeNumber {
      */
     public boolean isPalindrome(int x) {
         // TODO: implement
-        return false;
+        if ((x != 0 && x % 10 == 0) || (x < 0)) {
+            return false;
+        }
+        int reverted = 0;
+        while (reverted < x) {
+            int temp = x % 10;
+            x = x / 10;
+
+            reverted = reverted * 10 + temp;
+        }
+        if (reverted > x) {
+            reverted = reverted / 10;
+        }
+        return reverted == x;
     }
 
     // ========================================================================
